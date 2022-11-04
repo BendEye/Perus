af22decbbcc0325afd2594fb7644b47a72dc5c3e
Salers <64411613+Lindgey@users.noreply.github.com>
2022-11-04 15:39:49 +0100

    Update FlyA.java

--- src/main/java/me/bendeye/perus/check/impl/fly/FlyA.java
+++ src/main/java/me/bendeye/perus/check/impl/fly/FlyA.java
@@ -1,13 +1,15 @@
 package me.bendeye.perus.check.impl.fly;
 
 import me.bendeye.perus.check.Check;
+import me.bendeye.perus.data.PlayerData;
 
 /**
  * @author Lindgey
  * made on me.bendeye.perus.check.impl.fly
  */
 public class FlyA extends Check {
-    public FlyA() {
+
+    public FlyA(PlayerData data) {
         super(data);
     }
 }
