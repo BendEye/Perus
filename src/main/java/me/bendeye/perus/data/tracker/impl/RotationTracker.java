4be81b65578dcc41ee9aa1ae75dc802824408178
BendEye <Koolguyniceboy@gmail.com>
2022-11-05 22:39:11 -0400

    Merge remote-tracking branch 'Main/master'

--- src/main/java/me/bendeye/perus/data/tracker/impl/RotationTracker.java
+++ src/main/java/me/bendeye/perus/data/tracker/impl/RotationTracker.java
@@ -1,46 +1,25 @@
 package me.bendeye.perus.data.tracker.impl;
 
-import lombok.Getter;
 import me.bendeye.perus.data.PlayerData;
 import me.bendeye.perus.data.tracker.api.Tracker;
 import me.bendeye.perus.packet.Packet;
 
-@Getter
 public class RotationTracker extends Tracker {
-
-    private float yaw, pitch, yawAccel, pitchAccel, deltaYaw, lastDeltaYaw;
-    private float lastYaw, lastPitch, lastYawAccel, lastPitchAccel, deltaPitch , lastDeltaPitch;
+    private float yaw, pitch, yawAccel, pitchAccel;
+    private float lastYaw, lastPitch, lastyawAccel, lastpitchAccel;
 
     public RotationTracker(PlayerData data) {
         super(data);
     }
     @Override
     public void handlePreCheck(Packet packet) {
-        if (packet.isFlying()) {
-            lastYaw = yaw;
-            lastPitch = pitch;
-
-            lastDeltaYaw = deltaYaw;
-            lastDeltaPitch = deltaPitch;
-
-            lastYawAccel = yawAccel;
-            lastPitchAccel = pitchAccel;
-
-            if(packet.isRotation()) {
-                yaw = packet.getFloat().read(0);
-                pitch = packet.getFloat().read(1);
-
-                deltaYaw = Math.abs(yaw - lastYaw) % 360F;
-                deltaPitch = pitch - lastPitch;
-
-                yawAccel = Math.abs(deltaYaw - lastDeltaYaw);
-                pitchAccel = Math.abs(deltaPitch - lastDeltaPitch);
-            } else {
-                deltaYaw = deltaPitch = 0;
-            }
-
-
-
+        if (packet.isRotation()) {
+            final float deltaYaw = Math.abs(yaw - lastYaw);
+            final float deltaPitch = Math.abs(pitch - lastPitch);
+            final float deltayawAccel = Math.abs(yawAccel - lastpitchAccel);
+            final float deltapitchAccel = Math.abs(pitchAccel - lastpitchAccel);
+            this.lastYaw = yaw;
+            this.lastPitch = pitch;
 
 
         }
