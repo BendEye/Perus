b1330a579392bf4249ab646081cc8fa2b81941e0
BendEye <Koolguyniceboy@gmail.com>
2022-11-05 22:39:11 -0400

    Merge remote-tracking branch 'Main/master'

---
+++ src/main/java/me/bendeye/perus/data/tracker/impl/RotationTracker.java
@@ -0,0 +1,32 @@
+package me.bendeye.perus.data.tracker.impl;
+
+import me.bendeye.perus.data.PlayerData;
+import me.bendeye.perus.data.tracker.api.Tracker;
+import me.bendeye.perus.packet.Packet;
+
+public class RotationTracker extends Tracker {
+    private float yaw, pitch, yawAccel, pitchAccel;
+    private float lastYaw, lastPitch, lastyawAccel, lastpitchAccel;
+
+    public RotationTracker(PlayerData data) {
+        super(data);
+    }
+    @Override
+    public void handlePreCheck(Packet packet) {
+        if (packet.isRotation()) {
+            final float deltaYaw = Math.abs(yaw - lastYaw);
+            final float deltaPitch = Math.abs(pitch - lastPitch);
+            final float deltayawAccel = Math.abs(yawAccel - lastpitchAccel);
+            final float deltapitchAccel = Math.abs(pitchAccel - lastpitchAccel);
+            this.lastYaw = yaw;
+            this.lastPitch = pitch;
+
+
+        }
+    }
+
+    @Override
+    public void handlePostCheck(Packet packet) {
+
+    }
+}
